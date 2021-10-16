07529c18e0cb8dbbb9f9d6e2521900fb60702894
Nicholas Verrico <nicholas.verrico@icloud.com>
2021-10-16 11:50:49 -0400

    Initial Commit

---
+++ FaceIDDemo/FaceIDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDDemoApp.swift
+//  FaceIDDemo
+//
+//  Created by Nicholas Verrico on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
